daf9f568054f82b348ba4501201acc5589496e2c
chengmarc <last@live.ca>
2023-11-04 21:57:37 -0400

    disabled console in distribution

--- core-process/uwork_distribute.bat
+++ core-process/uwork_distribute.bat
@@ -1,2 +1,2 @@
-pyinstaller --distpath .\distribution --workpath .\distribution --onefile --add-data "uwork_icon.ico;." --add-data "uwork_report.txt;." --icon=uwork_icon.ico uwork_gui.py
+pyinstaller --distpath .\distribution --workpath .\distribution --onefile --noconsole --collect-all customtkinter --add-data "uwork_icon.ico;." --add-data "uwork_report.txt;." --icon=uwork_icon.ico uwork_gui.py
 pause
